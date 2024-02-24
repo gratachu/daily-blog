93c29ca19c536178a8f2a6adafd8b618ac019d0b
gratachu <akitatsu95.climbing@gmail.com>
2024-02-24 15:39:17 +0900

    Add: authentication by github credential

--- components/nav/Navbar.tsx
+++ components/nav/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import {Button} from "@/components/ui/button";
 
 import { SiGithub } from "react-icons/si";
+import LoginForm from "@/components/nav/LoginForm";
 
 export default function Navbar() {
   return (
@@ -10,10 +11,7 @@ export default function Navbar() {
         <Link href="/" className="text-2xl font-bold">Daily Blog</Link>
         <div className="h-1 w-0 group-hover:w-full transition-all bg-green-500"></div>
       </div>
-      <Button variant="outline" className="flex items-center gap-2">
-        <SiGithub />
-        Login
-      </Button>
+      <LoginForm />
     </nav>
   )
 }
