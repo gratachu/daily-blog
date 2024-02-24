469860477a7dfc8565cf2b6be8627a19940131b3
gratachu <akitatsu95.climbing@gmail.com>
2024-02-24 16:02:50 +0900

    Add: user state management

--- components/nav/Navbar.tsx
+++ components/nav/Navbar.tsx
@@ -1,17 +1,20 @@
-import Link from "next/link";
-import {Button} from "@/components/ui/button";
+"use client"
 
-import { SiGithub } from "react-icons/si";
+import Link from "next/link";
 import LoginForm from "@/components/nav/LoginForm";
+import {useUser} from "@/lib/store/user";
 
 export default function Navbar() {
+  const user = useUser((state) => state.user)
+  console.log(user)
+
   return (
     <nav className="flex items-center justify-between">
       <div className="group">
         <Link href="/" className="text-2xl font-bold">Daily Blog</Link>
         <div className="h-1 w-0 group-hover:w-full transition-all bg-green-500"></div>
       </div>
-      <LoginForm />
+      { user?.id ? <h1>Profile</h1> : <LoginForm />}
     </nav>
   )
 }
