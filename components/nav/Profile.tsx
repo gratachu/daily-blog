c751251717d75cef8ec00da00e0758363eb79ae2
gratachu <akitatsu95.climbing@gmail.com>
2024-02-26 19:01:47 +0900

    Add: protection to dashboard

--- components/nav/Profile.tsx
+++ components/nav/Profile.tsx
@@ -26,6 +26,8 @@ export default function Profile() {
     setUser(undefined)
   }
 
+  const isAdmin = user?.user_metadata?.role === "admin"
+
 
   return (
     <Popover>
@@ -43,12 +45,12 @@ export default function Profile() {
           <p>{user?.user_metadata?.user_name}</p>
           <p className="text-gray-500">{user?.user_metadata?.email}</p>
         </div>
-        <Link href="/dashboard" className="block">
+        { isAdmin && <Link href="/dashboard" className="block">
           <Button variant="ghost" className="w-full flex items-center justify-between">
             Dashboard
             <DashboardIcon />
           </Button>
-        </Link>
+        </Link>}
         <Button
           variant="ghost"
           className="w-full flex items-center justify-between"
