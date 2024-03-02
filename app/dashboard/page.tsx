4c3b0d5050a84109284d8db71a833a293b7a70a1
TatsuakiIto <gratachu9@gmail.com>
2024-03-02 18:25:01 +0900

    WIP: adding dashboard/blog/create page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,16 @@
+import {Button} from "@/components/ui/button";
+import {PlusIcon} from "@radix-ui/react-icons";
+import Link from "next/link";
+
 export default function Dashboard() {
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>This is my page</p>
+    <div className="flex items-center justify-between">
+      <h1 className="text-2xl font-bold">Blogs</h1>
+      <Link href="/dashboard/blog/create">
+        <Button variant="outline">
+          Create <PlusIcon />
+        </Button>
+      </Link>
     </div>
   )
 }
